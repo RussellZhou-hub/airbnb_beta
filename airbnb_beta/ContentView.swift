64516160668a14d6ff600a859c97d02e4b1bee9e
RussellZhou-hub <938311288@qq.com>
2024-02-21 05:29:01 +0800

    start dev

---
+++ airbnb_beta/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  airbnb_beta
+//
+//  Created by VincentZhou on 2024/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ airbnb_beta/airbnb_betaApp.swift
@@ -0,0 +1,17 @@
+//
+//  airbnb_betaApp.swift
+//  airbnb_beta
+//
+//  Created by VincentZhou on 2024/2/21.
+//
+
+import SwiftUI
+
+@main
+struct airbnb_betaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
